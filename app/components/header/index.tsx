5e2ccbfe231c5b6c069c618dcc56acc095b88d74
A <anthonyc.animba@gmail.com>
2023-11-25 10:50:20 -0800

    .

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className="header text-[#eee] flex justify-between items-center absolute top-0 left-0 p-6 py-9 w-full">
       <div className="logo flex items-center gap-3">
-        <a>Anthony Animba™</a>
+        <a>
+          Anthony Animba <span className="text-[80%]">™</span>{" "}
+        </a>
       </div>
       <div className="txt pr-12">
         <span className="rise">Designer / Developer</span>
