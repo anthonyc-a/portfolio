4eb149d238da285295473e58cf6cb7871b2f9e58
A <anthonyc.animba@gmail.com>
2023-11-25 11:22:39 -0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,15 @@ const Home = () => {
       <div className="w-full bg-[#1a1a1a] text-[#f5f5f5]">
         <Hero />
       </div>
-      <div className="about h-[100vh] p-6">
+      <div className="about h-[100vh] p-6 pt-12 grid grid-rows-1 grid-cols-3">
         <div className="txt">about me</div>
+        <h2>
+          I am passionate about everything that has to do with Digital Design
+          and Art Direction. I enjoy working with agencies and enthusiastic
+          people who want to solve problems through beautiful designs and
+          experiences.
+        </h2>
+        pic
       </div>
     </div>
   );
