7dbe51e396e28dd25c401aa74f7f79d5aea5fe41
A <anthonyc.animba@gmail.com>
2023-11-30 17:39:10 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import Header from "./components/header";
 import Cursor from "./components/cursor";
 import Footer from "./components/footer";
+import { CustomEase } from "gsap/all";
 
 export default function RootLayout({
   children,
@@ -31,11 +32,11 @@ export default function RootLayout({
         end: () => `+=${wrapper.offsetHeight}`, // Adjust end based on wrapper height
         scrub: true,
         onUpdate: (self) => {
-          const progress: any = self.progress.toFixed(2);
+          const progress: any = self.progress.toFixed(3);
           const friction = 2 - progress * 1;
 
           // Calculate the new Y position
-          const newYPos = -(progress * 100) * friction;
+          const newYPos = -(progress * 50) * friction;
 
           // Update the position only if it has changed
           if (newYPos !== yPos) {
@@ -43,7 +44,10 @@ export default function RootLayout({
             gsap.to(wrapper, {
               y: newYPos + "%",
               duration: 0.65,
-              ease: "expo.out",
+              ease: CustomEase.create(
+                "custom",
+                "M0,0 C0.084,0.61 0.033,0.846 0.1,0.9 0.175,0.962 0.374,1 1,1 "
+              ),
               overwrite: "auto", // Overwrite previous animations to avoid conflicts
             });
           }
