47f6e4ce3192a8438e0be93e1c9595389701268e
A <anthonyc.animba@gmail.com>
2023-11-30 17:13:11 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import "./css/globals.scss";
+import "./css/webfonts/britti-sans-variable.css";
+
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import "./css/globals.scss";
-import "./css/webfonts/britti-sans-variable.css";
+
 import Header from "./components/header";
 import Cursor from "./components/cursor";
 import Footer from "./components/footer";
@@ -17,24 +19,24 @@ export default function RootLayout({
   const wrapperRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-  
+
     const wrapper = wrapperRef.current;
-  
+
     if (wrapper) {
       let yPos = 0; // To store the current Y position
-  
+
       const scrollTrigger = ScrollTrigger.create({
         trigger: wrapper,
         start: "top top",
         end: () => `+=${wrapper.offsetHeight}`, // Adjust end based on wrapper height
         scrub: true,
         onUpdate: (self) => {
-          const progress:any = self.progress.toFixed(2);
+          const progress: any = self.progress.toFixed(2);
           const friction = 1 - progress * 1;
-  
+
           // Calculate the new Y position
           const newYPos = -(progress * 100) * friction;
-          
+
           // Update the position only if it has changed
           if (newYPos !== yPos) {
             yPos = newYPos;
@@ -42,27 +44,26 @@ export default function RootLayout({
               y: newYPos + "%",
               duration: 0.35,
               ease: "power5.out",
-              overwrite: 'auto' // Overwrite previous animations to avoid conflicts
+              overwrite: "auto", // Overwrite previous animations to avoid conflicts
             });
           }
         },
       });
-  
+
       return () => {
         scrollTrigger.kill();
       };
     }
   }, []);
-  
 
   return (
     <html lang="en">
       <body>
         <Cursor />
-        <Header />
         <div ref={wrapperRef} className="friction-wrapper">
+          <Header />
           {children}
-        <Footer />
+          <Footer />
         </div>
       </body>
     </html>
