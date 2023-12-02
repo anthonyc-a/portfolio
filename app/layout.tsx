8838b323a376af3b714dfb4aa60b021953a9e39c
A <anthonyc.animba@gmail.com>
2023-12-02 22:29:46 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
         </title>
         <meta
           name="description"
-          content="Digital Designer and Full-Stack Developer Currently based in London"
+          content="I'm a designer and developer currently based in London"
         />
         <meta name="author" content="Anthony Animba" />
         <meta name="keywords" content="Anthony Animba, Designer, Developer" />
