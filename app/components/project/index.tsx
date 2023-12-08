a42284bd5fbf138fcca50ae4f73e469c6892e950
A <anthonyc.animba@gmail.com>
2023-12-08 21:29:11 +0700

    .

--- app/components/project/index.tsx
+++ app/components/project/index.tsx
@@ -20,11 +20,11 @@ const projects = [
     tags: ["UI/UX", "Web Design", "Front-End", "Consultancy"],
     content: {
       brief:
-        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "The brief for this project was to make design changes to the website in whichever manner I deemed fit. I carefully analyzed the existing design and identified areas that required improvement. By utilizing my UI/UX design skills, I proposed and implemented changes that enhanced the website's visual appeal, user experience, and overall performance.",
       process:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "To fulfill the brief, I followed a systematic process. Firstly, I conducted a thorough review of the website's design and identified areas that needed improvement. I then generated design concepts and prototypes using Figma, ensuring alignment with the client's branding and target audience. Collaborating with the development team, I implemented the approved design changes using React, Gatsby, and SCSS. Throughout the process, I actively sought feedback from stakeholders and made iterative improvements based on their input.",
       outcome:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "By the end of the contract, I successfully made noticeable visual and performance improvements to the Double Agency website. These changes positively impacted various aspects, including search engine optimization (SEO), analytics, and load time. Additionally, my contributions helped the team make an informed decision regarding the best framework to use for their new website and future client projects.",
     },
   },
   {
@@ -39,11 +39,11 @@ const projects = [
     tags: ["Full-Stack", "API", "UI/UX", "Consultancy"],
     content: {
       brief:
-        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "The project's brief was to create a high-quality MVP application for Verve TV. The aim was to leverage Next.js, styled components, and Tailwind CSS to deliver a seamless and visually appealing user experience. Additionally, the application needed to integrate AWS for user management and streaming functionalities.",
       process:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "To meet the project's goals, I followed a structured process. Initially, I collaborated closely with the Verve TV team to understand their vision and requirements. Next, I developed wireframes and interactive prototypes using Figma, ensuring a user-centric design approach. Once the design was approved, I started implementing the MVP application using Next.js, styled components, and Tailwind CSS. Throughout the development process, I continuously tested and iterated on the features to ensure optimal performance and usability.",
       outcome:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "As a result of my contributions, Verve TV successfully launched its MVP application. The application showcased a seamless user interface, visually appealing design, and efficient streaming capabilities. The integration of AWS facilitated smooth user management and high-quality streaming experiences. The project's outcome exceeded expectations, setting a strong foundation for future development and growth of Verve TV.",
     },
   },
 
@@ -59,11 +59,11 @@ const projects = [
     tags: ["Web Design", "CMS", "Full-Stack", "Art Direction"],
     content: {
       brief:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "The brief for this project was to develop a temporary solution for archvizual.com, a renowned 3D designer's portfolio website. The goal was to showcase their work while their main website development was in progress and delayed. The solution needed to be lightweight and visually appealing, aligning with the designer's branding and design aesthetics.",
       process:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "To meet the project's goals, I followed a structured process. Firstly, I closely collaborated with the Archvizual team to understand their requirements and the designer's vision. I conducted a thorough analysis of the existing design assets and gathered insights to inform my design decisions. Leveraging Figma, React, Gatsby, Styled Components, and Contentful CMS, I developed a visually appealing and user-friendly portfolio website. Throughout the process, I maintained open communication with the client and incorporated their feedback to ensure the final product met their expectations.",
       outcome:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "As a result of my contributions, I successfully delivered a lightweight and visually appealing temporary portfolio website for Archvizual in under a week. The website effectively showcased the designer's work and maintained consistency with their desired branding and design aesthetics. By utilizing basic design principles with intuitive animations, I ensured a seamless user experience. The temporary solution served its purpose by providing an efficient platform for the designer to exhibit their work before their main website was complete.",
     },
   },
   {
@@ -78,11 +78,11 @@ const projects = [
     tags: ["UI/UX", "Web Design", "Front-End", "Art Direction"],
     content: {
       brief:
-        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "The brief for this project was to leverage my expertise in UI/UX design and front-end development to enhance Snapi's audience activation product. The goal was to implement new features that would improve user engagement and bridge any usability gaps through thoughtful and intuitive design.",
       process:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "To fulfill the brief, I followed a structured process. Firstly, I collaborated closely with the Snapi team to understand their product vision and user requirements. I conducted user research and gathered insights to inform the design and development process. Using Figma, I created wireframes and interactive prototypes that aligned with the user-centric design approach. Once the design was approved, I utilized React, Next.js, Styled Components, and Tailwind CSS to implement the front-end functionalities. Throughout the process, I continuously tested and iterated on the design and functionality to ensure a seamless and engaging user experience.",
       outcome:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.",
+        "As a result of my contributions, Snapi's audience activation product saw significant improvements in user engagement and usability. The new functionalities I implemented successfully enhanced the overall user experience and eliminated gaps in usability. By leveraging my skills in Figma, React, Next.js, Styled Components, and Tailwind CSS, I created an intuitive and visually appealing interface that drove user engagement and satisfaction.",
     },
   },
 ];
@@ -113,7 +113,7 @@ const Project = () => {
       {visibleProjects.map((project, index) => (
         <div
           key={index}
-          className="project bg-white py-6 md:py-8 md:pt-7  p-4 md:p-8"
+          className="project bg-white py-6 md:py-8 md:pt-7 max-h-[2000px] transition-all h-fit  p-4 md:p-8"
           onClick={() => handleProjectClick(index)}
         >
           <AnimationWrapper>
